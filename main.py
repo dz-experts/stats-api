eae5a13260b2b9803f0690d371751ce896b8d042
Walid ZIOUCHE <01walid@gmail.com>
2020-03-22 14:40:59 +0100

    Customize Swagger UI header and description

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
-from fastapi import FastAPI
 import requests
 
-app = FastAPI()
+from fastapi import FastAPI
+from starlette.responses import RedirectResponse
+
+app = FastAPI(
+    title="Algeria COVID19 API",
+    description="A simple RESTful API to get stats about COVID19 spread in Algeria (confirmed, recovered and deaths)",
+    version="0.0.4",
+)
 
 
 @app.get("/", include_in_schema=False)
 def read_root():
-    from starlette.responses import RedirectResponse
-    response = RedirectResponse(url='/docs')
+    response = RedirectResponse(url="/docs")
     return response
 
 
 @app.get("/stats")
 def read_stats():
+    """
+    Get stats
+    """
     url = "https://api.coronatracker.com/v2/analytics/country"
     r = requests.get(url=url)
     data = r.json()
@@ -23,4 +31,4 @@ def read_stats():
 def read_history():
     url = "https://corona.lmao.ninja/historical/Algeria"
     r = requests.get(url=url)
-    return r.json()['timeline']
+    return r.json()["timeline"]
