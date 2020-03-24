b395034febef45c50f23c144c2350917cd19d478
Sohaib LAFIFI <sohaib.lafifi@univ-artois.fr>
2020-03-24 18:06:26 +0100

    Add wilaya endpoint

--- main.py
+++ main.py
@@ -32,3 +32,26 @@ def read_history():
     url = "https://corona.lmao.ninja/historical/Algeria"
     r = requests.get(url=url)
     return r.json()["timeline"]
+
+
+
+@app.get("/wilayas")
+def read_history():
+    url = "https://services9.arcgis.com/jaH8KnBq5el3w2ZR/arcgis/rest/services/COVID_wilaya/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Report%20asc&resultOffset=0&resultRecordCount=1000&cacheHint=true"
+    r = requests.get(url=url)
+    data = list()
+    raw_data = list(r.json()["features"])
+
+    for wilaya in raw_data:
+        if(wilaya['attributes']['NOM_WILAYA']):
+            data.append({
+                'name' : wilaya['attributes']['NOM_WILAYA'],
+                'code' : wilaya['attributes']['WILAYA'],
+                'deaths' : wilaya['attributes']['Décés'],
+                'recovered' : wilaya['attributes']['Récupéré'],
+                'confirmed' : wilaya['attributes']['confirmé'],
+                'Female': wilaya['attributes']['Femelle'],
+                'Male': wilaya['attributes']['Male']
+            })
+    return data
+
